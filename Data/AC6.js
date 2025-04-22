a8ecacfafb7d6770e260dbde76391e0da3f2ec69
Daruka Eon <darukaeon@gmail.com>
2025-04-21 17:02:48 -0700

    Restructuring db parts.

--- Data/AC6.js
+++ Data/AC6.js
@@ -20,7 +20,7 @@ const PARTS = [
   };
   
   function loadCSVOptions(part) {
-    fetch(`AC6_db/ArmoredCore6_PartsDatabase_${part.file}.csv`)
+    fetch(`Parts/${part.file}.csv`)
       .then(res => res.text())
       .then(data => {
         const [header, ...lines] = data.trim().split('\n');
