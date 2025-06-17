cda1e26497ae7a4ab2b7698c37f3fbebc7ae5d5c
Daruka Eon <darukaeon@gmail.com>
2025-06-17 14:52:19 -0700

    Panic push.

--- Data/AC6.js
+++ Data/AC6.js
@@ -76,7 +76,7 @@ const PARTS = [
 ];
 
 const GOOGLE_SHEETS_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbxkcZREOnj4iBo32dRJBMsPIOUOrK1sANrX2lXmJzUNK7qG_XB8b5U4X_N5i_5wzr5qHg/exec";
-const BAD_WORDS = ["fuck", "pussy", "bitch", "asshole", "kys", "kill yourself", "dyke", "nigger", "nigga", "faggot", "fag", "f@g", "f@ggot", "@sshole", "cunt", "shit", "@ss", "retard"];
+const BAD_WORDS = ["bitch", "asshole", "@sshole", "shit", "@ss", "retard", "kys", "kill yourself", "fuck", "idiot", "cunt", "pussy", "dyke", "nigger", "nigga", "faggot", "fag", "f@g", "f@ggot"];
 
 window.onload = () => {
   PARTS.forEach(part => loadCSVOptions(part));
